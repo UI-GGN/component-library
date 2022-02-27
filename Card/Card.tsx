0608d2778d1e33a529a819456737f6b8eb6df7d4
Ravinder Mahajan <88770719+ravinder-tw@users.noreply.github.com>
2022-02-27 14:03:39 +0530

    temp

---
+++ Card/Card.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export interface CardProps {
+  variant: 'outlined' | 'elevated';
+  backgroundColor?: string;
+  children: React.ReactNode;
+}
+
+const Card = ({ variant, backgroundColor, children }: CardProps) => (
+  <div
+    className={`card-${variant}`}
+    style={{ backgroundColor: backgroundColor }}
+  >
+    {children}
+  </div>
+);
+
+export default Card;
